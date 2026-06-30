6d9345ea6b75232abe9fb474eb0c20611df77cc6
Abhimanyu Singh <158279512+abhimanyuBuilds@users.noreply.github.com>
2026-06-30 17:52:47 +0530

    Daily checkout file

---
+++ Topics/MERN_NOTES/dailyCheckOut.js
@@ -0,0 +1,68 @@
+/**
+ * @Array 
+ * 
+push()
+pop()
+shift()
+unshift()
+slice()
+splice()
+concat()
+sort()
+reverse()
+includes()
+indexOf()
+find()
+findIndex()
+filter()
+map()
+reduce()
+some()
+every()
+ */
+
+
+
+
+/**
+ * @String 
+ split()
+join()
+substring()
+slice()
+replace()
+replaceAll()
+trim()
+includes()
+startsWith()
+endsWith()
+repeat()
+charAt()
+charCodeAt()
+ */
+
+
+
+/**
+ * @Object 
+ Object.keys()
+Object.values()
+Object.entries()
+Object.assign()
+spread (...)
+destructuring
+ * 
+ */
+
+
+/**
+ * @ES6
+ let
+const
+arrow function
+rest operator
+spread operator
+template literals
+destructuring
+modules
+ */
